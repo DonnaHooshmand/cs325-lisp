ef78a5fb817a6b358fbee87c802be51135a1552e
Tiger Nie <nhl0819@gmail.com>
2020-10-19 00:57:53 +0800

    changes

--- bfs.lisp
+++ bfs.lisp
@@ -1,11 +1,22 @@
-(ql:quickload "cs325")
+(in-package :cs325-user)
+
+
+(defun super ()
+  (catch 'abort
+         (sub)
+         (format t "We'll never see this.")))
+(defun sub ()
+  (throw 'abort 99))
+
+(sub)
 
 (defun shortest-path (start end net)
   (catch 'abort
          (bfs end (list (list start)) net)))
 
 (defun bfs (end queue net)
-  (if (empty-queue-p queue)
+  ;(if (empty-queue-p queue)
+  (if (null queue)
       nil
       (let ((path (car queue)))
         (let ((node (car path)))
@@ -14,9 +25,32 @@
               (bfs end
                    (append (cdr queue)
                            (new-paths path node net))
-                   net)))))) 
+                   net))))))
+
+(trace bfs)
+(untrace)
+(trace new-paths)
 
 (defun new-paths (path node net)
   (mapcar #'(lambda (n)
               (cons n path))
           (cdr (assoc node net))))
+
+(shortest-path 'a 'd '((a b c) (b c) (c d)))
+
+(test-path '(a c d) 'a 'd '((a b c) (b c) (c d)))
+(test-path '(a c) 'a 'c '((a b c) (b c) (c d)))
+(test-path '(a c d) 'a 'd '((a b c) (b e) (e c) (c d)))
+(test-path '(a c e f) 'a 'f '((a b c) (b c) (c e) (e f)))
+(test-path '(a b f) 'a 'f '((a b c) (b c f) (c e) (e f)))
+(test-path '()  'a 'f '((a b c) (b c) (c d) (e f)))
+(test-path '(a b f) 'a 'f '((a b c d) (b e f)))
+
+;; With cycles
+(test-path '(a c d) 'a 'd '((a b c) (b a c) (c d)))
+(test-path '(a c) 'a 'c '((a b c) (b c) (c a d)))
+(test-path '() 'a 'c '((a b) (b a) (c)))
+(test-path '() 'a 'f '((a b c) (b a c) (c d) (e f)))
+(test-path '() 'a 'f '((a b c) (b c) (c b) (e f)))
+(test-path '(a b c e f) 'a 'f '((a b) (b c d) (c e) (d a) (e f)))
+
