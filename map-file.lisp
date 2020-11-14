3b1bb28f6d6b4fb920767002e99d28e1a217663b
Tiger Nie <nhl0819@gmail.com>
2020-11-15 02:12:06 +0800

    improved map-file

--- map-file.lisp
+++ map-file.lisp
@@ -1,17 +1,20 @@
-
 (defun map-stream (f str)
   (let ((eof (list 'eof)))
     (do ((line (read str nil eof)
                (read str nil eof)))
         ((eql line eof) nil)
-      (funcall f line))))
+        (funcall f line))))
+
+(do ((line (read str nil eof)
+           (read str nil eof)))
+    ((eql line eof) nil)
+    (funcall f line))
+
 
 (defun map-file (f file)
   (with-open-file (str file :direction :input)
-    (let ((eof (list 'eof)))
-    (do ((line (read-line str nil eof)
-               (read-line str nil eof))
-         (res nil (cons (funcall f line) res)))
-        ((eql line eof) nil)))))
+    (map-stream f str)))
 
-(map-file #'print "~/.zshrc")
+(map-file #'print "~/.sbclrc")
+
+(run-tests map-stream)
